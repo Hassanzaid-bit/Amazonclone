a8359c3c85c844af9d007932339919f0850e2fa3
hassanzaid-bit <hzaid.hz@gmail.com>
2021-07-30 08:16:08 +0300

    ..

---
+++ src/components/Header/Header.js
@@ -0,0 +1,10 @@
+import { mdiAmazon } from '@mdi/js';
+import amazon from './amazon.svg'
+
+const Header = () => {
+    return ( 
+        <nav> <img src={amazon} alt="" /></nav>
+     );
+}
+ 
+export default Header;
---
+++ src/components/Home/Home.js
@@ -0,0 +1,7 @@
+
+
+const Home = () => {
+    return ( <div className="home">Hello World again</div> );
+}
+ 
+export default Home;
